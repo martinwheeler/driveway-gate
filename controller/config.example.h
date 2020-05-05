4ac263389ea78de5200c676aed62b508d3f7ff4d
Martin Wheeler <martin@martinwheeler.com.au>
2020-05-05 10:38:45 +1000

    fix: update wifi connection timing

---
+++ controller/config.example.h
@@ -0,0 +1,4 @@
+#define SSID "MyWifi"
+#define SECRET "password"
+#define SLACK_WEBHOOK "https://hooks.slack.com/services/.../.../..."
+#define SLACK_HTTPS_FINGERPRINT "..." // HTTPS Fingerprint SHA1 that matches with the HTTPS certificate from the slack hook address
