ee101fda5163ba58bbc526939ac720ce95f6da3e
Martin Wheeler <martin@martinwheeler.com.au>
2020-05-20 12:48:08 +1000

    Revert "feat: add extra app responses"
    This reverts commit 15b1e46b5702bde466af6852a6cbfc8af376a9b4.

--- controller/config.example.h
+++ controller/config.example.h
@@ -2,4 +2,3 @@
 #define SECRET "password"
 #define SLACK_WEBHOOK "https://hooks.slack.com/services/.../.../..."
 #define SLACK_HTTPS_FINGERPRINT "..." // HTTPS Fingerprint SHA1 that matches with the HTTPS certificate from the slack hook address
-#define AUTHORIZATION_VALUE "Bearer some-unique-value-here"
