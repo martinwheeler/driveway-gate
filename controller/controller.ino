b3193e438468e0653dcfa3368031c006f8492ebb
Martin Wheeler <martin@martinwheeler.com.au>
2020-05-20 12:48:15 +1000

    Revert "refactor: move loop logic to separate functions"
    This reverts commit 01b97ee3a73840fb6bad2e9ddca37e83fbd24df4.

--- controller/controller.ino
+++ controller/controller.ino
@@ -13,7 +13,7 @@ using namespace std;
 #include "Utils.h"
 
 const int IS_DEBUG = 1;
-const int IS_LIGHT_ENABLED = 1;
+const int IS_LIGHT_ENABLE = 1;
 const String VERSION = "1.0.0";
 
 HTTPClient http;
@@ -26,60 +26,73 @@ const int GATE_DIRECTION = 14;      // Cable colour: BROWN, Digital Pin: D5
 const int TRIGGER_PIN = 5;          // Cable colour: BLACK, Digital Pin: D3
 const int LIGHT_PIN = 4;            // Cable colour: ORANGE, Digital Pin: D4
 
-int openTrigger = LOW, previousConnectionStatus, lightOffTime;
-bool hasGateOpened = false, gateOpening = false, gateClosing = false, attemptingWifiConnection = false, firstConnection = true, lightsOn = false;
+int openTrigger = LOW;
+bool hasGateOpened = false, gateOpening = false, gateClosing = false, attemptingWifiConnection = false;
 
 const int OPEN_DURATION = 70000; // 70 seconds
 const int LEFT_CLOSE_DELAY = 4750; // 4.75 seconds
 const int RIGHT_OPEN_DELAY = 350; // 0.35 seconds
 const int MOVING_DURATION = 12000; // 12 seconds
-const int CONNECTION_TIMEOUT = 30000; // 30 seconds
-const int LIGHT_OFF_TIMEOUT = 30000; // 30 seconds
 
-unsigned long currentTime = 0, gateOpeningTime = 0, gateClosingTime = 0, connectionStartTime = 0;
+unsigned long currentTime = 0, gateOpeningTime = 0, gateClosingTime = 0;
 
 vector<String> unsentMessages;
 
-void connectWifi() {
-  if (WiFi.status() == WL_CONNECTED) {
+void connectWifi(int retries = 200) {
+  int tries = 0;
+  attemptingWifiConnection = true;
+
+  WiFi.begin(SSID, SECRET);
+
+  if (IS_DEBUG) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.print(SSID);
+    Serial.println();
+    Serial.print("Connecting");
+  }
+
+  while (WiFi.status() != WL_CONNECTED) {
+    if (IS_DEBUG) {
+      Serial.print(".");
+    }
+    
+    delay(500);
+ 
+    tries++;
+    if (tries >= retries) {
+      attemptingWifiConnection = false;
+      break;
+    }
+  }
+
+  if (tries >= retries) {
+    attemptingWifiConnection = false;
     return;
   }
 
-//  if (IS_DEBUG) {
-//    Serial.print("Attempting to connect to SSID: ");
-//    Serial.println(SSID);
-//  }
-
-  connectionStartTime = millis();
-  WiFi.begin(SSID, SECRET);  
+  if (IS_DEBUG) {
+    Serial.println("\r\n");
+  }
+  
+  printWifiData();
 }
 
-void printWifiData(bool reconnected = false) {
+void printWifiData() {
   IPAddress ip = WiFi.localIP();
   String ipStr = String(ip[0]) + "." + String(ip[1]) + "." + String(ip[2]) + "." + String(ip[3]);
 
-  String wifiPayload = "*=======================================================================*\r\n";
-  wifiPayload += "*Network Connection* :computer::signal_strength:\r\n";
-  wifiPayload += reconnected ? "Reconnected\r\n" : "";
-  wifiPayload += "\r\nSSID: " + String(SSID);
-  wifiPayload += "\r\nIP: " + ipStr;
-  wifiPayload += "\r\nSignal: " + String(WiFi.RSSI()) + " dBm";
-  wifiPayload += "\r\n*=======================================================================*";
+  String wifiPayload = "Network Connection :computer::signal_strength:\r\n";
+  wifiPayload += "*--------------------------------------------*\r\n";
+  wifiPayload += "SSID: ";
+  wifiPayload += SSID;
+  wifiPayload += "\r\n";
+  wifiPayload += "IP: ";
+  wifiPayload += ipStr;
+  wifiPayload += "\r\n";
+  wifiPayload += "Signal: " + String(WiFi.RSSI()) + " dBm";
+  wifiPayload += "\r\n*--------------------------------------------*";
 
   logMessage(wifiPayload);
-}
-
-void printInformation() {
-  String informationPayload = "*=======================================================================*\r\n";
-  informationPayload += "*Config* :bulb:\r\n";
-  informationPayload += "\r\nVersion: " + VERSION;
-  informationPayload += "\r\nLights: ";
-  informationPayload += (IS_LIGHT_ENABLED) ? "Yes" : "No";
-  informationPayload += "\r\nClose Delay: " + String(OPEN_DURATION / 1000) + " seconds";
-  informationPayload += "\r\nLight Off Delay: " + String(LIGHT_OFF_TIMEOUT / 1000) + " seconds";
-  informationPayload += "\r\n*=======================================================================*";
-
-  logMessage(informationPayload);
 }
 
 /**
@@ -88,6 +101,10 @@ void printInformation() {
  * @param String message - The message that will be sent.
  */
 void logMessage(String message) {
+  if (IS_DEBUG) {
+    Serial.println(message);
+  }
+
   if (WiFi.status() == WL_CONNECTED) {
     int responseCode = 0;
     client->setFingerprint(SLACK_HTTPS_FINGERPRINT);
@@ -102,7 +119,7 @@ void logMessage(String message) {
     if (IS_DEBUG) {
       int messageLength = measureJson(jsonDoc);
   
-      Serial.print("Payload Size: ");
+      Serial.print("Size: ");
       Serial.println(messageLength);  
     }
     
@@ -117,98 +134,88 @@ void logMessage(String message) {
     http.addHeader("Content-Type", "application/json");
     responseCode = http.POST(slackPayload);
 
-    if (IS_DEBUG && responseCode < 0) {
+    if (responseCode < 0 && IS_DEBUG) {
       Serial.printf("[HTTPS] POST... failed, error: %s\n", http.errorToString(responseCode).c_str());
     }
     
     http.end();
   } else {
-    if (IS_DEBUG) {
-     Serial.println("\r\nSaving message:\r\n"); 
-    }
-    
+    Serial.println("\r\n Saving unsent message \r\n");
     unsentMessages.push_back(message);
-  }
-
-  if (IS_DEBUG) {
-    Serial.println(message);
   }
 }
 
-bool startedOpening = false, startedOpeningRight = false;
+void printInformation() {
+  String informationPayload = "Config :bulb:\r\n";
+  informationPayload += "*--------------------------------------------*\r\n";
+  informationPayload += "\r\nVersion: " + VERSION;
+  informationPayload += "\r\nLights: ";
+  informationPayload += (IS_LIGHT_ENABLE) ? "Yes" : "No";
+  informationPayload += "\r\nClose Delay: " + String(OPEN_DURATION / 1000) + "seconds";
+  informationPayload += "\r\n*--------------------------------------------*";
+
+  logMessage(informationPayload);
+}
 
 void openGate() {
   currentTime = millis() - gateOpeningTime;
 
-  if (!startedOpening) {
-    logMessage("The front gate is beginning to open.");
+  if (currentTime < 100) {
     digitalWrite(GATE_DIRECTION , HIGH);
     digitalWrite(LEFT_MOTOR , HIGH);
-    startedOpening = true;
+
+    if (IS_LIGHT_ENABLE) {
+      digitalWrite(LIGHT_PIN, HIGH); // Turn on the lights now that the gates are opening
+    }
+
+    logMessage("The front gate is beginning to open.");
   }
 
-  if (startedOpening && currentTime > RIGHT_OPEN_DELAY && !startedOpeningRight) {
+  if (currentTime > RIGHT_OPEN_DELAY && currentTime < (RIGHT_OPEN_DELAY + 100)) {
     digitalWrite(RIGHT_MOTOR , HIGH);
-    startedOpeningRight = true;
   }
 
-  if (currentTime > MOVING_DURATION && startedOpeningRight) {
+  if (currentTime > MOVING_DURATION) {
     digitalWrite(RIGHT_MOTOR , LOW);
     digitalWrite(LEFT_MOTOR , LOW);
     digitalWrite(GATE_DIRECTION , LOW);
-    logMessage("The front gate is open.");
     hasGateOpened = true;
     gateOpening = false;
   }
 }
 
-bool startedClosing = false, startedClosingLeft = false;
-
-void closeGate () {
+void closeGate () {  
   currentTime = millis() - gateClosingTime;
 
-  if (!startedClosing) {
-    logMessage("The front gate is beginning to close.");
+  // Begin closing the gate
+  if (currentTime < 100) {
+    digitalWrite(GATE_DIRECTION , LOW);
     digitalWrite(RIGHT_MOTOR , HIGH);
-    startedClosing = true;
+
+    logMessage("The front gate is beginning to close.");
   }
 
-  if (startedClosing && currentTime > LEFT_CLOSE_DELAY && !startedClosingLeft) {
-    digitalWrite(LEFT_MOTOR , HIGH);
-    startedClosingLeft = true;
+  if (currentTime > LEFT_CLOSE_DELAY && currentTime < (LEFT_CLOSE_DELAY + 100)) {
+    digitalWrite(LEFT_MOTOR , HIGH); 
   }
 
-  if (currentTime > MOVING_DURATION && startedClosingLeft) {
+  if (currentTime > MOVING_DURATION) {
     digitalWrite(RIGHT_MOTOR , LOW);
     digitalWrite(LEFT_MOTOR , LOW);
-    logMessage("The front gate is closed.");
     hasGateOpened = false;
     gateClosing = false;
   }
 }
 
 void handleLights () {
-  if (gateOpening && !startedOpening) {
-    logMessage("Turning on the lights.");
-    digitalWrite(LIGHT_PIN, HIGH); // Turn on the lights
-    lightsOn = true;
-  }
 
-  // Always update this while the gate is closing, once closed we then count down from that time.
-  if (gateClosing && (lightsOn && !gateOpening)) {
-    lightOffTime = millis();
-  }
-
-  if (!hasGateOpened && ((millis() - lightOffTime) > LIGHT_OFF_TIMEOUT) && lightsOn && !gateOpening) {
-    digitalWrite(LIGHT_PIN, LOW); // Turn off the lights
-    logMessage("Turning off the lights.");
-    lightsOn = false;
-  }
+  // TODO: Toggle lights off or on with a time delay
+  
 }
 
 void setup(){
   if (IS_DEBUG) {
-    Serial.begin(9600);
+    Serial.begin(9600); // open the serial port at 9600 bps:
     while (!Serial) {
       return; // wait for serial port to connect. Required for debugging.
     }
@@ -220,7 +227,9 @@ void setup(){
     Serial.println();
   }
 
-  connectWifi();
+  while (WiFi.status() != WL_CONNECTED) {
+    connectWifi();
+  }
   
   pinMode(RIGHT_MOTOR, OUTPUT);
   pinMode(LEFT_MOTOR, OUTPUT);
@@ -233,72 +242,47 @@ void setup(){
   digitalWrite(RIGHT_MOTOR, LOW);
   digitalWrite(LIGHT_PIN, LOW);
 }
+  
+void loop(){
+  openTrigger = digitalRead(TRIGGER_PIN);
 
-void handleUnsentMessages () {
-  if (unsentMessages.size() > 0 && WiFi.status() == WL_CONNECTED) {    
-    auto it = unsentMessages.begin();
-    while (it != unsentMessages.end()) {
-      logMessage(*it);
-      it = unsentMessages.erase(it);
+  if (!attemptingWifiConnection && (WiFi.status() != WL_CONNECTED)) {
+    connectWifi();
+  }
+
+  if (unsentMessages.size() > 0 && WiFi.status() == WL_CONNECTED) {
+    Serial.println("Sending unsent messages");
+    
+    for (const String &message : unsentMessages) {
+      logMessage(message);
     }
   }
-}
 
-void handleGateTrigger () {
   if (openTrigger == LOW && !hasGateOpened && !gateOpening) {
-    startedOpening = false;
-    startedOpeningRight = false;
     gateOpening = true;
     gateOpeningTime = millis();
   }
   
   if (openTrigger == HIGH && hasGateOpened && !gateClosing && (millis() - gateOpeningTime > OPEN_DURATION)) {
-    startedClosing = false;
-    startedClosingLeft = false;
     gateClosing = true;
     gateClosingTime = millis();
   }
 
   if (openTrigger == LOW && gateClosing && (millis() - gateOpeningTime > 100)) {
-    startedOpening = false;
-    startedOpeningRight = false;
     gateOpening = true;
     gateClosing = false;
     gateOpeningTime = millis();
   }
-}
 
-void loop(){
-  openTrigger = digitalRead(TRIGGER_PIN);
-
-  if (previousConnectionStatus != WL_CONNECTED && WiFi.status() == WL_CONNECTED && !firstConnection) {
-    printWifiData(true);
-  }
-  previousConnectionStatus = WiFi.status();
-
-  if (firstConnection && (WiFi.status() == WL_CONNECTED)) {
-    printWifiData();
-    printInformation();
-    firstConnection = false;
-  }
-
-  if (WiFi.status() != WL_CONNECTED && (millis() - connectionStartTime > CONNECTION_TIMEOUT)) {
-    connectWifi();
-  }
-
-  handleGateTrigger();
-
-  if (IS_LIGHT_ENABLED) {
-    handleLights(); 
-  }
+  handleLights();
 
   if (gateOpening) {
     openGate();
+    return;
   }
 
   if (gateClosing) {
     closeGate();
+    return;
   }
-
-  handleUnsentMessages();
 }
